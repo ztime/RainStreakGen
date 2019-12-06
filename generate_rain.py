c540b1e8244b0d3011dd1928a958fb0ea19cdcf4
ztime <jonas@lechuck.se>
2019-12-06 15:47:48 +0100

    lets see what it does

--- generate_rain.py
+++ generate_rain.py
@@ -3,6 +3,7 @@ import sys
 import argparse
 import numpy as np
 import cv2
+import random
 import glob
 from skimage.filters import gaussian
 from skimage.util import img_as_float,img_as_uint
@@ -37,12 +38,87 @@ def main():
 
     all_frames = np.zeros((args.number_of_frames, args.dimX, args.dimY, args.img_channels))
     for frame_index in range(args.number_of_frames):
+        print(f"Working on {frame_index}/{args.number_of_frames}...")
         available_images = glob.glob(f"{args.streak_folder}/*-{args.angle}.png")
         for _ in range(iterations): # We dont need to keep track
+            random_streak_img = random.choice(available_images)
+            random_streak_img = cv2.imread(random_streak_img)
+            # Crop 3 pixel rows to get a picture that can scale evenly
+            random_streak_img = random_streak_img[3:, :, :]
+            # Flip the width and height here, not sure why
+            resize_shape = random_streak_img.shape
+            resize_shape = (resize_shape[1] * 4, resize_shape[0] * 4)
+            random_streak_img_resized = cv2.resize(random_streak_img, resize_shape)
+            # put filtered channels in a new frame
+            new_frame = np.zeros_like(random_streak_img_resized, dtype=np.float)
+            thresholded_frame = imbinarize_O(cv2.cvtColor(random_streak_img_resized, cv2.COLOR_BGR2GRAY))
+            frame_mask = bwareafilt(thresholded_frame)
+            for img_channel in range(3):
+                one_channel = random_streak_img_resized[:,:,img_channel]
+                one_channel = np.multiply(one_channel, frame_mask.astype(np.uint8))
+                # No idea about these settings, from the original
+                one_channel = gaussian(one_channel, 1, truncate=2)
+                new_frame[:,:,img_channel] = one_channel
+            # Take the size down again
+            new_frame = cv2.resize(new_frame, (args.dimX, args.dimY))
+            # Add it to the frame with a few modifications
+            alpha = random.random() * 0.2 + 0.25
+            filtered_frame = new_frame * alpha
+            all_frames[frame_index] = all_frames[frame_index] + (new_frame.astype(float) * alpha)
+        frame_prepend_zeros = len(str(args.number_of_frames))
+        filename = os.path.join(args.output, f"{args.prefix}-{frame_index:0>{frame_prepend_zeros}}.png")
+        imsave(filename, img_as_uint(all_frames[frame_index]))
 
+    print(f"Done with {args.number_of_frames} frames!")
 
+def bwareafilt(image,min_size=0,max_size=1000,connectivity=4):
+    image = image.astype(np.uint8)
 
+    # test_image = np.copy(image).astype(np.float)
+    # test_image[test_image >= np.average(image) - 1.0] = 1.0
+    # test_image[test_image < np.average(image) - 1.0] = 0.0
+    # print(image.shape)
+    # print(np.amax(image))
+    # print(np.amin(image))
+    # print(np.average(image))
 
+    # fig, axes = plt.subplots(1,3)
+    # axes[0].imshow(image)
+    nb_components, labels , stats, _ = cv2.connectedComponentsWithStats(image, connectivity=connectivity)
+    # print(f"nb_components:{nb_components}")
+    # print(f"labels:{labels}")
+    # print(f"labels shape:{labels.shape}")
+    # print(f"stats:{stats}")
+    # print(f"stats shape:{stats.shape}")
+    # print(f"stats max: {np.amax(stats)}")
+    sizes = stats[:, -1]
+    # print(f"sizes:{sizes}")
+    # print(f"sizes shape:{sizes.shape}")
+    img2 = np.zeros(labels.shape)
+    label = 1
+    for i in range(2,nb_components):
+        # print(f"Running for {i}/{nb_components}...")
+        if sizes[i] <= max_size and sizes[i]>=min_size:
+            # print(f"Size was ok!")
+            label = i
+            img2[labels == label] = 1
+        # fig = plt.figure()
+        # plt.imshow(img2)
+        # plt.show()
+        # plt.close(fig)
+    # axes[1].imshow(img2)
+    # axes[2].imshow(test_image)
+    # plt.show()
+    # plt.close(fig)
+    quit()
+    return img2
+
+#Otsu's Binarization
+def imbinarize_O(img, GaussianBlur=False):
+    if GaussianBlur:
+        img = cv2.GaussianBlur(img,(5,5),0)
+    _,th = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    return th
 
 
     # Algorithm:
@@ -68,9 +144,6 @@ def main():
                         # apply some gaussian noise to the channel
                     # Resize back to original with and height
                     # 
-
-                        
-    
 
 if __name__ == '__main__':
     main()
