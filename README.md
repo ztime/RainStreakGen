584447a83e2dc5b61cc59e01946d2d70c2c96057
ztime <jonas@lechuck.se>
2019-12-21 09:49:29 +0100

    cleanup and updated readme

--- README.md
+++ README.md
@@ -1,5 +1,39 @@
 # Rain streak generator
 
-Based on an implementation of Rain Streaks Generation based on Paper [Photorealistic Rendering of Rain Streaks](http://www1.cs.columbia.edu/CAVE/publications/pdfs/Garg_TOG06.pdf)
-by [yxinjiang](https://www.github.com/yxinjiang). This implementation is very simplified though!
+Based on the [Photorealistic Rendering of Rain Streaks](http://www1.cs.columbia.edu/CAVE/publications/pdfs/Garg_TOG06.pdf)
+and using implementation from [Yixin Juang](https://www.github.com/yxinjiang) and [Ruoteng Li](https://github.com/liruoteng). 
+
+This implementation is very simplified though! It only generates rain streaks on solid black background
+
+Some options and how to use it:
+```
+usage: generate_rain.py [-h] -o OUTPUT [-n NUMBER_OF_FRAMES] [-p PREFIX]
+                        [-x DIMX] [-y DIMY] [--streak_folder STREAK_FOLDER]
+                        [--img_channels IMG_CHANNELS]
+                        [--intensity {dense,middle,light}]
+                        [--angle {4,5,6,7,8}]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -o OUTPUT, --output OUTPUT
+                        Folder to store results in
+  -n NUMBER_OF_FRAMES, --number_of_frames NUMBER_OF_FRAMES
+                        How many frames to generate
+  -p PREFIX, --prefix PREFIX
+                        Prefix of rendered images
+  -x DIMX, --dimX DIMX
+  -y DIMY, --dimY DIMY
+  --streak_folder STREAK_FOLDER
+                        Where the dataset of rain is stored
+  --img_channels IMG_CHANNELS
+                        How many channels to generate
+  --intensity {dense,middle,light}
+  --angle {4,5,6,7,8}
+```
+
+## Requirements
+* Python 3.6+
+* OpenCV
+* skimage
+* Pyplot
 
--- generate_rain.py
+++ generate_rain.py
@@ -23,7 +23,6 @@ def main():
     parser.add_argument('--intensity', type=str, choices=['dense', 'middle','light'], default='dense')
     parser.add_argument('--angle', type=int, default=4, choices=[4,5,6,7,8])
     args = parser.parse_args()
-    # parser.add_argument('-p', '--prefix', help='Prefix for the files', required=True)
     # the last digit in the image '158-5.png' is an angle, which varies between 4 and 8.
     # thes means 10-4.png and 165-4.png are connected bc it's the same angle.
     if not os.path.isdir(args.output):
@@ -89,31 +88,6 @@ def imbinarize_O(img, GaussianBlur=False):
         img = cv2.GaussianBlur(img,(5,5),0)
     _,th = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     return th
-
-
-    # Algorithm:
-    # For each image to add rain to:
-        # Load file, get image,filename, height?, width?
-            # for every angle X in 4,5,6,7,8:
-                #copy image as floats between [0, 1] (no idea if its widthxheightx3 or how many channels)
-                #create two zeroed images numpy arras w x h x 3
-                #get a list of all images for angle X
-                # for i in range 8: (this is probably bc we want DENSE) alter i depending on how intense
-                # it should be
-                    #Pick a random image for angle X and load it as n
-                    #remove the first 2 rows? n = n[3:,:,:] n goes from (568 x 320 x 3?) -> (565,320, 3?)
-                    # resize n into (565 * 4, 320 * 4) -> (2260, 1280)
-                    # another zeros array same shape as n called new_n
-                    # Convert n from RGB to grayscale BGR2GRAY -> img
-                    # run imbinarize_0 on img -> bwst THIS IS SOMEKIND OF THRESHOLD! masky masky!
-                    # create a mask bwareafilt(bwst) of any components that are connected
-                    # but not to big
-                    # For each color channel (3) as c:
-                        #isolate the channel t = n[:,:,c]
-                        # t = t * mask i.e only keep that which are not background ish
-                        # apply some gaussian noise to the channel
-                    # Resize back to original with and height
-                    # 
 
 if __name__ == '__main__':
     main()
